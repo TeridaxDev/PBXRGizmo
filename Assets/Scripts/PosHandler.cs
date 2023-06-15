a975335613b80a889ff7a23ce5116b0a1591ec81
TeridaxDev <haileymott66@gmail.com>
2023-06-15 19:48:30 -0400

    implement translate

--- Assets/Scripts/PosHandler.cs
+++ Assets/Scripts/PosHandler.cs
@@ -4,5 +4,27 @@ using UnityEngine;
 
 public class PosHandler : TransformHandler
 {
-    
+    public override void HandleDragEvent(Vector3 mouseOld, Vector3 mouseNew)
+    {
+        Ray cameraRay = Camera.main.ScreenPointToRay(mouseNew);
+
+        //The offset required to make the positionhandler track the mouse, instead of the gizmo's origin
+        Vector3 originGizmoDistance = gizmoOrigin.position - this.transform.position;
+        //The direction this handler is facing; most likely one of the world axes
+        Vector3 worldPositive = (this.transform.position - gizmoOrigin.position).normalized;
+        //ray from the gizmo's origin to this handler's axis
+        Ray positiveRay = new Ray(gizmoOrigin.transform.position, worldPositive);
+        //finds the shortest distance from the mouse postion to the above ray, then returns the distance between that point and the ray's origin
+        float closestT = ClosestPointOnRay(positiveRay, cameraRay);
+        //the amount of distance to translate
+        Vector3 hitPoint = positiveRay.GetPoint(closestT);
+
+        //offsets the destination position by the distance between this object and the center of the gizmo
+        Vector3 offset = hitPoint + originGizmoDistance - gizmoOrigin.position;
+
+        Vector3 position = gizmoOrigin.position + offset;
+
+        gizmoOrigin.parent.position = position;
+
+    }
 }
--- Assets/Scripts/TransformHandler.cs
+++ Assets/Scripts/TransformHandler.cs
@@ -6,7 +6,39 @@ public class TransformHandler : MonoBehaviour
 {
     //This should be 1,0,0; 0,1,0; or 0,0,1
     //Filters the input to just one axis
-    [SerializeField] private Vector3 influence;
+    [SerializeField] protected Vector3 influence;
+    //Reference to the gizmo's origin point
+    [SerializeField] protected Transform gizmoOrigin;
+
+    public const float PRECISION_THRESHOLD = 0.001f;
+
+    //I found this helper function here: https://github.com/pshtif/RuntimeTransformHandle (MIT liscense)
+    //it finds the point on vector AB (ray) that is closest in distance to any point on ray CD (other), and returns the distance between that point and A
+    //it's crucial for aligning the end user's mouse movements to the axis they are trying to manipulat in world space
+    public static float ClosestPointOnRay(Ray ray, Ray other)
+    {
+        // based on: https://math.stackexchange.com/questions/1036959/midpoint-of-the-shortest-distance-between-2-rays-in-3d
+        // note: directions of both rays must be normalized
+        // ray.origin -> a
+        // ray.direction -> b
+        // other.origin -> c
+        // other.direction -> d
+
+        float bd = Vector3.Dot(ray.direction, other.direction);
+        float cd = Vector3.Dot(other.origin, other.direction);
+        float ad = Vector3.Dot(ray.origin, other.direction);
+        float bc = Vector3.Dot(ray.direction, other.origin);
+        float ab = Vector3.Dot(ray.origin, ray.direction);
+
+        float bottom = bd * bd - 1f;
+        if (Mathf.Abs(bottom) < PRECISION_THRESHOLD)
+        {
+            return 0;
+        }
+
+        float top = ab - bc + bd * (cd - ad);
+        return top / bottom;
+    }
 
     public virtual void HandleDragEvent(Vector3 mouseOld, Vector3 mouseNew) { }
 }
